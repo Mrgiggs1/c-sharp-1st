624a400a3dac8d29ca966a54a1c84e0168079d86
Mrgiggs1 <rudolphpuane@gmail.com>
2022-05-18 15:58:28 +0200

    member inserted

--- ADO6ConsoleApp/Program.cs
+++ ADO6ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AccessLayer;
+using System.Data.SqlClient;
 namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
 {
     internal class Program
@@ -8,8 +9,8 @@ namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
         {
             //this is a connection string which is used to store the details.
             string connect = "Server= LAPTOP-ICUOINHF; Database = AFCentral;Trusted_Connection=true;";
-
-            
+            var con = new SqlConnection(connect);
+            con.Open();
             //create query that writes to database
             string dep,opt;
             Console.WriteLine("Press The Following Options \n1: Read Data or 2: Insert Data");
@@ -21,13 +22,35 @@ namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
                     // code block
                     Console.WriteLine("\nWhich Department you Looking for:");
                     dep = Console.ReadLine();
-                    Access.readData(dep, connect);
+                    Access.readData(dep, con);
                     break;
                 case 2:
                     // code block
+                    Console.WriteLine("Enter FirstName");
+                    string fName = Console.ReadLine();
+
+                    Console.WriteLine("Enter Surname");
+                    string lName = Console.ReadLine();
+
+                    Console.WriteLine("Enter Full Name");
+                    string fullName = Console.ReadLine();
+
+                    Console.WriteLine("Enter your SA ID");
+                    string ID = Console.ReadLine();
+
+                    Console.WriteLine("Enter your Parking Spot No.");
+                    string parkNo = Console.ReadLine();
+
+                    Console.WriteLine("Do you celebrate Birthdays?");
+                    string isBirthday = Console.ReadLine();
+
                     Console.WriteLine("Write down Name of the department");
                     dep = Console.ReadLine();
-                    Access.insertData(dep, connect);
+
+                    Console.WriteLine("Write down your Positon Name");
+                    string positionName = Console.ReadLine();
+
+                    Access.insertData(fName, lName, fullName, ID, parkNo,isBirthday, positionName,dep , con);
                     break;
                 default:
                     // code block
