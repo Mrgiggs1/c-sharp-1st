63d14159cdd0336a17740b8a10f6fd670deacbab
Mrgiggs1 <rudolphpuane@gmail.com>
2022-05-19 15:34:10 +0200

    try and catch

--- ADO6ConsoleApp/Program.cs
+++ ADO6ConsoleApp/Program.cs
@@ -16,24 +16,38 @@ namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
             Console.WriteLine("Press The Following Options \n1: Search Data or 2: Insert Data");
             opt = Console.ReadLine();
 
-            switch (Convert.ToInt32(opt))
+            try
             {
-                case 1:
-                    // function for reading values
-                    search(con);
-                    break;
-                case 2:
-                    // function for entering values
-                    enterValues(con);
-                    break;
-                default:
-                    // code block
-                    Console.WriteLine("Default to Searching Data");
-                    search(con);
-                    break;
+                switch (Convert.ToInt32(opt))
+                {
+                    case 1:
+                        // function for reading values
+                        search(con);
+                        break;
+                    case 2:
+                        // function for entering values
+                        enterValues(con);
+                        break;
+                    default:
+                        // code block
+                        Console.WriteLine("Default to Searching Data");
+                        search(con);
+                        break;
+                }
             }
+            catch(Exception err)
+            {
+                Console.WriteLine(err);
+                Console.WriteLine("-------------------\n" +
+                                    "Provide correct Data" +
+                                    "\n-------------------");
+            }
+
+            
         }
 
+
+        //enter what you need to search on the database
         public static void search(SqlConnection con)
         {
             string search;
@@ -42,6 +56,7 @@ namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
             Access.readData(search, con);
         }
 
+        //entering data values to insert 
         public static void enterValues(SqlConnection con)
         {
                 string dep, opt;
@@ -68,7 +83,7 @@ namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
                 Console.WriteLine("Write down your Positon Name");
                 string positionName = Console.ReadLine();
 
-                //@fName,@lName,@fullName,@identity,@parkNo,@depID,@posID,@isBirthDay
+                //access insert function
                 Access.insertData(fName, lName, fullName, ID, parkNo, isBirthday, positionName, dep, con);
         }
 
